77e8df9e0de46d675929095b75eb828b3bb42027
cyChopp <45396059+cyChopp@users.noreply.github.com>
2020-11-29 22:05:26 +0100

    persistent login

--- src/SignUp/SignUp.js
+++ src/SignUp/SignUp.js
@@ -1,4 +1,4 @@
-import { TextField } from "@material-ui/core";
+import { CircularProgress, TextField } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 
 import React, { useEffect, useState } from "react";
@@ -15,23 +15,21 @@ const SignUp = () => {
   const [emailError, setEmailError] = useState("");
   const [passwordError, setPasswordError] = useState("");
   const [hasAccount, setHasAccount] = useState(false);
-  const [pending,setPending] = useState(true)
+  const [pending, setPending] = useState(true);
 
-  const clearInputs = ()=>{
-    setEmail('');
-    setPassword('');
-  }
+  const clearInputs = () => {
+    setEmail("");
+    setPassword("");
+  };
 
-  const clearErrors = ()=>{
-    setEmailError('');
-    setPasswordError('');
-  }
+  const clearErrors = () => {
+    setEmailError("");
+    setPasswordError("");
+  };
 
   const { register, handleSubmit, errors, control } = useForm();
 
-
   const handleSignIn = (data) => {
-   
     debugger;
     clearErrors();
     console.log(data);
@@ -48,17 +46,17 @@ const SignUp = () => {
             setPasswordError(errors.message);
             break;
 
-            default:
-              return console.log('other case error')
+          default:
+            return console.log("other case error");
         }
       });
-      setEmail(data.email);
-      setPassword(data.password)
+    setEmail(data.email);
+    setPassword(data.password);
   };
   const handleSignUp = (data) => {
     debugger;
 
-    clearErrors()
+    clearErrors();
     console.log(data);
     db.auth()
       .createUserWithEmailAndPassword(data.email, data.password)
@@ -72,134 +70,148 @@ const SignUp = () => {
             setPasswordError(errors.message);
             break;
 
-            default:
-              return console.log('other case error')
-        
+          default:
+            return console.log("other case error");
         }
       });
 
     setEmail(data.email);
-    setPassword(data.password)
+    setPassword(data.password);
   };
 
   const handleLogOut = () => {
     db.auth().signOut();
   };
 
-// user Exists?
-  const authListener = ()=>{
-    db.auth().onAuthStateChanged(user=>{
-      if(user){
+  // user Exists?
+  const authListener = () => {
+    db.auth().onAuthStateChanged((user) => {
+      if (user) {
         clearInputs();
-        setUser(user)
-        setPending(false)
-      }else{
-        setUser('')
+        setUser(user);
+      } else {
+        setPending(false);
+        setUser("");
       }
-    })
-  }
+    });
+  };
 
-
-  useEffect(()=>{
-    authListener()
-    console.log(user)
-  },[])
+  useEffect(() => {
+    authListener();
+  }, []);
 
   // if(pending){
   //   return <>Loading...</>
   // }
 
-
   return (
     <FeedWrapper>
       <StickyTop header={"Sign Up"} />
-      
-      <div className="container__wrapper ">
-      {pending ? (<>Loading...</>) :
-        <div className="container">
-        {user ? (<div onClick={handleLogOut}>Log Out</div>) :(
-          <form onSubmit={handleSubmit(hasAccount ? (handleSignIn) : (handleSignUp) )}>
-            <Controller
-              name="email"
-              ref={register}
-              render={(props) => (
-                <TextField
-                  color="secondary"
-                  required
-                  {...props}
-                  label="email"
-                  InputLabelProps={{ shrink: true }}
-                />
-              )}
-              control={control}
-              defaultValue=""
-            />
-            <p>{emailError}</p>
-            <Controller
-              name="password"
-              ref={register}
-              render={(props) => (
-                <TextField
-                  color="secondary"
-                  {...props}
-                  required
-                  label="password"
-                  InputLabelProps={{ shrink: true }}
-                />
-              )}
-              control={control}
-              defaultValue=""
-            />
-            <p>{passwordError}</p>
-            <Controller
-              name="passwordConfirmation"
-              ref={register}
-              render={(props) => (
-                <TextField
-                  color="secondary"
-                  {...props}
-                  label="Confirm password"
-                  InputLabelProps={{ shrink: true }}
-                />
-              )}
-              control={control}
-              defaultValue=""
-            />
-            <Controller
-              name="username"
-              ref={register}
-              render={(props) => (
-                <TextField
-                  color="secondary"
-                  required
-                  {...props}
-                  label="username"
-                  InputLabelProps={{ shrink: true }}
-                />
-              )}
-              control={control}
-              defaultValue=""
-            />
-                  {hasAccount ?(
-                      <>
-                        <Button type="submit" color="secondary">Sign In</Button>
-                        <p>Don't have an account? <span onClick={()=>setHasAccount(!hasAccount)}>Sign Up</span></p>
-                      </>
-                  ):(
-                    <>
-                        <Button type="submit" color="secondary">Sign Up</Button>
-                        <p>Have already an account? <span onClick={()=>setHasAccount(!hasAccount)}>Sign in</span> </p>
-                      </>
+
+      {pending ? (
+        <CircularProgress color="secondary" size={50} />
+      ) : (
+        <div className="container__wrapper ">
+          <div className="container">
+            {user ? (
+              <div onClick={handleLogOut}>Log Out</div>
+            ) : (
+              <form
+                onSubmit={handleSubmit(
+                  hasAccount ? handleSignIn : handleSignUp
+                )}
+              >
+                <Controller
+                  name="email"
+                  ref={register}
+                  render={(props) => (
+                    <TextField
+                      color="secondary"
+                      required
+                      {...props}
+                      label="email"
+                      InputLabelProps={{ shrink: true }}
+                    />
                   )}
-            {/* <Button type="submit" color="secondary">
-              {hasAccount ? 'Sign In' : 'Sign Up' }
-            </Button> */}
-          </form>
-        )}
+                  control={control}
+                  defaultValue=""
+                />
+                <p>{emailError}</p>
+                <Controller
+                  name="password"
+                  ref={register}
+                  render={(props) => (
+                    <TextField
+                      color="secondary"
+                      {...props}
+                      required
+                      label="password"
+                      InputLabelProps={{ shrink: true }}
+                    />
+                  )}
+                  control={control}
+                  defaultValue=""
+                />
+                <p>{passwordError}</p>
+                <Controller
+                  name="passwordConfirmation"
+                  ref={register}
+                  render={(props) => (
+                    <TextField
+                      color="secondary"
+                      {...props}
+                      label="Confirm password"
+                      InputLabelProps={{ shrink: true }}
+                    />
+                  )}
+                  control={control}
+                  defaultValue=""
+                />
+                <Controller
+                  name="username"
+                  ref={register}
+                  render={(props) => (
+                    <TextField
+                      color="secondary"
+                      required
+                      {...props}
+                      label="username"
+                      InputLabelProps={{ shrink: true }}
+                    />
+                  )}
+                  control={control}
+                  defaultValue=""
+                />
+                {hasAccount ? (
+                  <>
+                    <Button type="submit" color="secondary">
+                      Sign In
+                    </Button>
+                    <p>
+                      Don't have an account?{" "}
+                      <span onClick={() => setHasAccount(!hasAccount)}>
+                        Sign Up
+                      </span>
+                    </p>
+                  </>
+                ) : (
+                  <>
+                    <Button type="submit" color="secondary">
+                      Sign Up
+                    </Button>
+                    <p>
+                      Have already an account?{" "}
+                      <span onClick={() => setHasAccount(!hasAccount)}>
+                        Sign in
+                      </span>{" "}
+                    </p>
+                  </>
+                )}
+              </form>
+            )}
+          </div>
         </div>
-      }
-      </div>
-      
+      )}
     </FeedWrapper>
   );
 };
